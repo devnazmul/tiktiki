d74c97bfe7f9938df8f678e02e2c8253302bc3e2
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2022-09-21 21:01:30 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/Layout";
-import Home from "./pages/Home";
+
 function App() {
   return (
     <Layout>
