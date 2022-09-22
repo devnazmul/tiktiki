17da04b6e847d058298a27999f524aaa33704d3c
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-09-22 17:54:38 +0600

    Design mobile Nav

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,17 +1,37 @@
 import React from "react";
+import { AiTwotoneHome } from "react-icons/ai";
+import { BsPlusSquareDotted } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { MdFiberNew, MdTextsms } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 import NavBar from "./LayoutComponents/NavBar/NavBar";
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <NavBar />
-      {/* ===== START MAIN =====  */}
-      <div className="main-containeer flex">
-        <div className="sidebar-container w-2/12 h-screen  bg-emerald-500"></div>
-        <div className="main w-10/12 h-screen bg-amber-400">{children}</div>
+    <div className="main w-full h-screen bg-amber-400">
+      <div className="sidebar-container rounded-t-2xl flex justify-around items-center w-full h-16  bg-white absolute bottom-0 left-0 text-2xl text-gray-700 z-10">
+        <NavLink to={`/`} className={'text-xs font-mono flex justify-center items-center flex-col'}>
+          <AiTwotoneHome className="text-2xl" />
+          Home
+        </NavLink>
+        <NavLink to={`/`} className={'text-xs font-mono flex justify-center items-center flex-col'}>
+          <MdFiberNew className="text-2xl" />
+          New
+        </NavLink>
+        <NavLink to={`/`} className={'text-xs font-mono flex justify-center items-center flex-col'}>
+          <BsPlusSquareDotted className="text-3xl" />
+        </NavLink>
+        <NavLink to={`/`} className={'text-xs font-mono flex justify-center items-center flex-col'}>
+          <MdTextsms className="text-2xl" />
+          Inbox
+        </NavLink>
+        <NavLink to={`/`} className={'text-xs font-mono flex justify-center items-center flex-col'}>
+          <CgProfile className="text-2xl" />
+          Profile
+        </NavLink>
       </div>
-      {/* ===== END MAIN =====  */}
-
+      <NavBar />
+      <>{children}</>
     </div>
   );
 }
--- src/layout/LayoutComponents/NavBar/NavBar.jsx
+++ src/layout/LayoutComponents/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ import NavSeaarch from '../NavSeaarch/NavSeaarch'
 
 export default function NavBar() {
   return (
-    <div className="nav-bar w-full h-20 flex justify-between">
+    <div className="nav-bar w-full h-20 flex justify-between bg-white z-50">
     <Logo LogoUrl={'./logo.png'} brandName={'TikTiki'} />
     <NavSeaarch />
     <NavMenu />
